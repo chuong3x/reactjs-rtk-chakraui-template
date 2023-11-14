363605a4482329f940bd8f25afadb86eb8ed737c
ChuongDo <chuongdotu@gmail.com>
2023-11-14 14:53:18 +0700

    config chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.tsx";
+
+import { store } from "./app/store.ts";
+import theme from "./theme/index.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
+        </Provider>
+    </React.StrictMode>
+);
